20a1b27127df5fcf36354ccd9bb7dc24135de170
despair1 <eveguide0@gmail.com>
2016-05-20 23:26:05 +0500

    1

---
+++ Assets/ship.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ship : MonoBehaviour {
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawLine(transform.position, cam.ScreenToWorldPoint(Input.mousePosition), Color.green);
+	
+	}
+}
