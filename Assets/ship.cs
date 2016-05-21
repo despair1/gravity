07c120d811c070a1affbac1ba89fa4f3973493d9
despair1 <eveguide0@gmail.com>
2016-05-21 13:41:29 +0500

    look at mouse point 2d

--- Assets/ship.cs
+++ Assets/ship.cs
@@ -12,6 +12,13 @@ public class ship : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Debug.DrawLine(transform.position, cam.ScreenToWorldPoint(Input.mousePosition), Color.green);
+        Vector3 lookAt = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        //lookAt.z = 0;
+        lookAt = lookAt - transform.position;
+        Debug.Log("screen2world" + lookAt.x + "_" + lookAt.y + "_" + lookAt.z);
+        Quaternion q = Quaternion.LookRotation(lookAt,new Vector3(0,0,1));
+        transform.rotation = q;
 	
 	}
 }
