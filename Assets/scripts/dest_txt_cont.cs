0a7971db0d9896b6588ea5777d8dc5367df6feb9
despair1 <eveguide0@gmail.com>
2016-05-26 22:37:18 +0500

    begin gravity interaction
    Signed-off-by: despair1 <eveguide0@gmail.com>

--- Assets/scripts/dest_txt_cont.cs
+++ Assets/scripts/dest_txt_cont.cs
@@ -3,11 +3,11 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class dest_txt_cont : MonoBehaviour {
-    private Text txt;
-    private dest_gui_cont dest_img;
+    protected Text txt;
+    protected dest_gui_cont dest_img;
     
 	// Use this for initialization
-	void Awake () {
+	virtual protected void Awake () {
         dest_img = GameObject.Find("dest_img").GetComponent<dest_gui_cont>();
         txt = gameObject.GetComponent<Text>();
 	
@@ -19,7 +19,7 @@ public class dest_txt_cont : MonoBehaviour {
 	
 	}
 
-    void set_range()
+    protected virtual void set_range()
     {
         txt.text = "Destinatio1n\n" + ((int)(dest_img.dest.transform.position -
             dest_img.player.transform.position).magnitude).ToString();
---
+++ Assets/scripts/velocity_txt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class velocity_txt : dest_txt_cont
+{
+    private float speed = 0;
+    private Rigidbody2D  player_rb;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        player_rb = dest_img.player.GetComponent<Rigidbody2D>();
+    }
+
+    protected override void set_range()
+    {
+        txt.text = "speed\n" + ((int)(speed)).ToString() +
+            "\n"+(int)player_rb.velocity.magnitude+"\n";
+    }
+}
